62b7471c01273a560c0d1a09ec934e4bebd180bd
codewithrama <ramasubramanian35427@outlook.com>
2026-06-24 06:22:48 +0530

    fixed styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,9 @@ import Layout from "../components/Layout";
 import {
   ArrowBigDown,
   ArrowBigUp,
+  Calendar,
   CookingPot,
+  ListFilterPlus,
   PiggyBankIcon,
   Plane,
   Plus,
@@ -14,7 +16,7 @@ export default function Dashboard() {
   const { income } = useData();
   return (
     <Layout>
-      <>
+      <div>
         <div className={styles.dashboardTopActions}>
           <div className={styles.dashboardHeadings}>
             <h2>Dashboard Overview</h2>
@@ -46,45 +48,88 @@ export default function Dashboard() {
 
           <div className={styles.totalIncomeCard}>
             <ArrowBigDown />
-            <h3>{income}</h3>
+            <h3>Total Income</h3>
+            <progress value={550} max={2600} className={styles.totalIncome} />
+            <p>30% Target Reached</p>
           </div>
 
           <div className={styles.totalExpenses}>
             <ArrowBigUp />
-            <h3>60</h3>
-            <input type="progress" max={income} value={60} />
-            <p>Well , within ${60} your limit</p>
+            <h3>Total expense</h3>
+            <progress
+              max={2600}
+              value={60}
+              className={styles.totalExpense}
+            ></progress>{" "}
+            <p>Well , within ${2600} your limit</p>
           </div>
         </div>
 
         <div className={styles.spendingByCategoryCard}>
-          <div className={styles.viewAll}> viewAll</div>
+          <div className={styles.viewAll}> View All</div>
           <div className={styles.cardItem}>
             <label>
               <CookingPot /> Food
             </label>
-            <progress max={income} value={30} />
+            <progress max={income} value={30} className={styles.progressFood} />
           </div>
           <div className={styles.cardItem}>
             <label>
               <Plane /> Travelling
             </label>
-            <progress max={income} value={20} />
+            <progress
+              max={income}
+              value={20}
+              className={styles.progressTravelling}
+            />
           </div>
           <div className={styles.cardItem}>
             <label>
-              <Receipt /> Food
+              <Receipt /> Bills and Utilities
             </label>
-            <progress max={income} value={200} />
+            <progress
+              max={income}
+              value={200}
+              className={styles.progressRecipt}
+            />
           </div>
         </div>
 
         <div className={styles.recentTransactionsCard}>
           <div className={styles.topActions}>
-            <div className={styles.Icons}></div>
+            <div className={styles.Icons}>
+              <h3>Recent Transactions</h3>
+            </div>
+            <ListFilterPlus />
+            <Calendar />
           </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Transcations</th>
+                <th>Amount </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Apple Store Soho</td>
+                <td>2000</td>
+              </tr>
+              <tr>
+                <td>Stripe Payout</td>
+                <td>8500</td>
+              </tr>
+
+              <tr>
+                <td>Blue Hill Restaurent</td>
+                <td>-342</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-      </>
+      </div>
     </Layout>
   );
 }
