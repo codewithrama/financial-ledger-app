af1fd0a03598f55ff06211aab32cb8ea8206d852
codewithrama <ramasubramanian35427@outlook.com>
2026-06-25 06:35:34 +0530

    json fake server

--- src/context/AuthContextProvider.jsx
+++ src/context/AuthContextProvider.jsx
@@ -1,37 +1,60 @@
-import {createContext,useState} from 'react'
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 export const AuthContext = createContext();
 
-export default function AuthContextProvider({children}){
-    const [customers,setCustomers] = useState([]);
-    const [formData,setFormData] = useState({
-        'name':'',
-        'email':'',
-        'passWord': '',
-        'id': null,
-        'isOnboardingDone':false,
-        'terms':false,
-        'income':null
-        
-    })
+export default function AuthContextProvider({ children }) {
+  const [customers, setCustomers] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    passWord: "",
+    id: null,
+    isOnboardingDone: false,
+    terms: false,
+    income: null,
+  });
 
-    //Signup flow
+  const BASE_URL = "http://localhost:5001";
 
-    function addUser(newCustomer){
-        if(!customers.includes(newCustomer.email)){
-            setCustomers((cus)=> [...cus,newCustomer])
-        } else{
-            toast.error('Already user exists')
-        }
+  //Signup flow
+  async function addUser(newCustomer) {
+    //preventing duplicate signUp
+    const hasCustomerDuplicate = customers.some(
+      (customer) => customer.email === newCustomer.email,
+    );
 
+    if (hasCustomerDuplicate) {
+      toast.info("Already account exists , Please Sign in");
+      return;
     }
-    return(
-        <AuthContext.Provider value ={{
-            formData,
-            setFormData,
-            addUser
-        }}>
-            {children}
 
-        </AuthContext.Provider>
-    )
+    try {
+      const data = await fetch(`${BASE_URL}/customers`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCustomer),
+      });
+
+      const res = await data.json();
+      setCustomers((prev) => prev, res);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  //sigin flow
+
+  return (
+    <AuthContext.Provider
+      value={{
+        formData,
+        setFormData,
+        addUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,52 +4,55 @@ import { React, useState } from "react";
 import styles from "./Signup.module.css";
 import { Eye, EyeOff } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import useAuth from '../hooks/useAuth'
-import {toast} from 'react-toastify';
+import useAuth from "../hooks/useAuth";
+import { toast } from "react-toastify";
 
 export default function Signup() {
-
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  const {formData,setFormData,addUser} = useAuth();
+  const { formData, setFormData, addUser } = useAuth();
 
-  const handleChange = (e)=> {
-    console.log(e.target.checked
-    )
-    const {name,value,checked} = e.target;
-    console.log(checked)
-    setFormData((form)=> ({
-      ...form,[name]:value || checked
-  }))
-}
+  const handleChange = (e) => {
+    console.log(e.target.checked);
+    const { name, value, checked } = e.target;
+    console.log(checked);
+    setFormData((form) => ({
+      ...form,
+      [name]: value || checked,
+    }));
+  };
 
-async function handleSignup (e){
+  async function handleSignup(e) {
     e.preventDefault();
-     if(formData.name.trim() === '' || formData.email.trim() === '' || formData.passWord.trim() === ''){
-      toast.error('Missing fields');
+    if (
+      formData.name.trim() === "" ||
+      formData.email.trim() === "" ||
+      formData.passWord.trim() === ""
+    ) {
+      toast.error("Missing fields");
       return;
     }
-    if(!formData.terms) {toast.error('Please accept Terms and Conditions'); return}
+    if (!formData.terms) {
+      toast.error("Please accept Terms and Conditions");
+      return;
+    }
 
     const newData = {
-      name : formData.name,
-      email : formData.email,
-      passWord:formData.passWord,
-      id: crypto.randomUUID(),
-      isOnboardingDone:false,
-      terms:true
-    }
-    console.log(newData)
+      name: formData.name,
+      email: formData.email,
+      passWord: formData.passWord,
+      id: `Led-${crypto.randomUUID().substring(0, 4)}`,
+      isOnboardingDone: false,
+      terms: true,
+      income: null,
+    };
+    console.log(newData);
 
-    await addUser(newData)
-    toast.success('Account Created Successfully !')
-    navigate('/onBoarding',{replace:true})
-
+    await addUser(newData);
+    toast.success("Account Created Successfully !");
+    navigate("/onBoarding", { replace: true });
   }
-
-
-
 
   return (
     <div className={styles.pageContainer}>
@@ -72,12 +75,12 @@ async function handleSignup (e){
             <h3>Create Account</h3>
             <p>Begin your journey toward finanical clarity.</p>
 
-            <form className={styles.signupForm} onSubmit = {handleSignup}>
+            <form className={styles.signupForm} onSubmit={handleSignup}>
               <div className="fullname">
                 <label>Full Name</label>
                 <input
                   type="text"
-                  name ='name'
+                  name="name"
                   className={styles.input}
                   value={formData.name}
                   onChange={handleChange}
@@ -89,12 +92,11 @@ async function handleSignup (e){
                 <label>Email Address</label>
                 <input
                   type="email"
-                  name ='email'
+                  name="email"
                   className={styles.input}
                   placeholder="julian@ledger.com"
-                  value = {formData.email}
+                  value={formData.email}
                   onChange={handleChange}
-
                 />
               </div>
               <div className={styles.passwordField}>
@@ -105,10 +107,9 @@ async function handleSignup (e){
                     type={showPassword ? "text" : "password"}
                     className={styles.input}
                     placeholder="******"
-                    name ='passWord'
-                    value ={formData.password}
+                    name="passWord"
+                    value={formData.password}
                     onChange={handleChange}
-
                   />
 
                   <span
@@ -122,15 +123,22 @@ async function handleSignup (e){
 
               <div className="checkbox">
                 <p>
-                  <input type="checkbox" value={formData.agree} onChange={handleChange} name ='terms'/>I agree to the{" "}
-                  <span>Terms of Service</span> and <span>Privacy Policy.</span>
+                  <input
+                    type="checkbox"
+                    value={formData.agree}
+                    onChange={handleChange}
+                    name="terms"
+                  />
+                  I agree to the <span>Terms of Service</span> and{" "}
+                  <span>Privacy Policy.</span>
                 </p>
               </div>
 
               <button type="submit">Create Account</button>
             </form>
             <p>
-              Already have an account ? <span onClick={()=> Navigate('/')}>Login</span>
+              Already have an account ?{" "}
+              <span onClick={() => navigate("/")}>Login</span>
             </p>
           </div>
         </div>
@@ -138,5 +146,4 @@ async function handleSignup (e){
       <Footer />
     </div>
   );
-
 }
